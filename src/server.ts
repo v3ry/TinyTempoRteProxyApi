914312e4d92204b38eed57ccc0fe629b472cf979
v3ry <v3ryfr@gmail.com>
2023-12-15 02:16:52 +0100

    first version 0.1

---
+++ src/server.ts
@@ -0,0 +1,122 @@
+import cors from 'cors'
+import express, { json } from 'express'
+import { get } from 'http'
+import { privateApiKey } from './secrets'
+
+const app = express()
+const CronJob = require("node-cron");
+
+app.use(express.json())
+app.use(cors())
+
+let initScheduledJobs = () => {
+  const scheduledJobFunction = CronJob.schedule("* * 7 * * *", () => {
+    console.log("Execution tache planifié récupération des données");
+    getToken().then(value=>{
+      getTempoInfo(value).then(result=> theResult = result);
+    });  
+  });
+
+  scheduledJobFunction.start();
+}
+
+initScheduledJobs();
+let theResult;
+
+app.set('trust proxy', true)
+
+app.get('/', (req, res) => res.send('🏠 Tempo Service By v3ry3D 🏠'))
+app.get('/tempo', (req, res) => getTempo(req,res))
+
+
+getToken().then(value=>{
+  getTempoInfo(value).then(result=> theResult = result);
+});  
+
+async function getTempo(req,res){
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  console.debug("api request by : " + ip);
+ res.send(theResult)
+}
+
+async function getToken() : Promise<any>{
+    try{
+      const myHeaders = new Headers();
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+      myHeaders.append('Authorization', 'Basic '+ privateApiKey);
+      //the URL of the website to which the content must be posted is passed as a parameter to the fetch function along with specifying the method, body and header
+      let response = await fetch('https://digital.iservices.rte-france.com/token/oauth/', {
+          method: 'POST',
+          credentials: 'include',
+          headers: myHeaders,
+          })
+          .then(response=>response.json())
+          .then((rese:any)=> rese["access_token"])
+      return await response;
+  }catch (error) {
+    if (error instanceof Error) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+  
+}
+
+async function getTempoInfo(token: string) : Promise<any>{
+  try{
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+    myHeaders.append('Authorization',"Bearer " + token);
+    console.log("token sent : " + token);
+    let response = await fetch('https://digital.iservices.rte-france.com/open_api/tempo_like_supply_contract/v1/tempo_like_calendars?start_date=' + getDate(true) + "&end_date="+ getDate(false), {
+        method: 'GET',
+        credentials: 'include',
+        headers: myHeaders,
+        })
+        .then(response=>response.json())
+        .then((rese:any)=> rese.tempo_like_calendars.values.map(val=> val.value))
+        .then(value=> value.map(toto=> {
+          if(toto == "BLUE"){
+              return 1
+            }else if(toto == "WHITE"){
+              return 2
+            }else if(toto == "RED"){
+              return 3
+            }else{
+              return 0
+            }
+        }))
+        console.log(response.length);
+        if( await response.length == 2){
+          response = {today: response[1], tomorow: response[0], hp: 6, hc: 22}
+        }else{
+          response = {today: response[0], tomorow: 0, hp: 6, hc: 22}
+        }
+        
+    return await response;
+  }catch (error) {
+    if (error instanceof Error) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+function getDate(startDate:boolean): string{
+  let date = new Date();
+  let dateString;
+  if(startDate){
+    dateString = date.getFullYear() + "-" + (date.getMonth()+1)+ "-" + date.getDate() + "T00:00:00%2B02:00"
+    return dateString;
+  }else{
+    dateString = date.getFullYear() + "-" + (date.getMonth()+1)+ "-" + (date.getDate()+2) + "T01:00:00%2B02:00"
+    return dateString;
+  }
+}
+
+app.listen(3000, () => console.log('ReTempo Api Serveur V1 Started'))
